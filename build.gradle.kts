5e6ec4ae9e73256baca62a44d7198a6030c88820
LorenzoEsposito <lorenzoesposito80@gmail.com>
2024-03-06 11:39:49 +0100

    First commmit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    application
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
 
 repositories {
     mavenCentral()
@@ -16,6 +18,11 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
+}
+
+application {
+    mainClass.set("MainKt")
 }
