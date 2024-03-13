9365a138ddb9cb623cc928689a57547126f7e55a
LorenzoEsposito <lorenzoesposito80@gmail.com>
2024-03-13 10:44:35 +0100

    HdrImage update

--- src/main/kotlin/HdrImage.kt
+++ src/main/kotlin/HdrImage.kt
@@ -5,18 +5,18 @@ class HdrImage (var width: Int,var height: Int) {
     /**
      * Initialize image with dimension width*height
      */
-    var pixels = Array(width*height) {10} //da aggiungere la funzione Color()
+    var pixels = Array(width*height) {Color(1.0F,1.0F ,1.0F)} //da aggiungere la funzione Color()
     /**
      * Get color of a pixel
      */
-    fun getPixel(x: Int, y: Int): Int {
+    fun getPixel(x: Int, y: Int): Color {
         return pixels[y * width + x]
     }
 
     /**
      * Set Color of a pixel
      */
-    fun setPixel(x: Int, y: Int, new_color: Int ) {  //da cambiare argomento di tipo color
+    fun setPixel(x: Int, y: Int, new_color: Color ) {  //da cambiare argomento di tipo color
         pixels[y * width + x] = new_color
     }
 
