717b6dd439a8ee06513cc47a0715841b7349fb2a
Giovanni Pellegrini <gio.pelle1124@gmail.com>
2024-05-14 09:37:05 +0200

    Add plane shape, missing tests

--- src/main/kotlin/plane.kt
+++ src/main/kotlin/plane.kt
@@ -1,3 +1,5 @@
+import kotlin.math.floor
+
 class plane(val transformation: Transformation = Transformation(),
             val origin: Point = Point(),
             val vector: Vector = Vector()): Shape{
@@ -10,6 +12,48 @@ class plane(val transformation: Transformation = Transformation(),
                                                       Vector(0f,0f,1f))
 
     override fun rayIntersection(ray: Ray): HitRecord? {
+        // compute t
+        val invRay = ray.transformation(transformation.inverse())
+        val origin = invRay.origin.toVec()
+        var t: Float? = null
+
+        // control if there is the intersection.
+        if (invRay.dir.z == 0f) {
+            t = -(origin.z / invRay.dir.z)
+        } else return null
+        if (t < invRay.tMin || t > invRay.tMax) return null
+
+        //no
+
+
+        val normal = if (invRay.dir.z < 0) transformation * Normal(0f, 0f, -1f)
+        else transformation * Normal(0f, 0f, 1f)
+
+        val hitPoint = invRay.at(t)
+
+        val hit = HitRecord(
+            worldPoint = hitPoint,
+            normal = normal,
+            surfacePoint = Vec2d(hitPoint.x- floor(hitPoint.x),hitPoint.y- floor(hitPoint.y)),
+            t= t,
+            ray=ray)
+        return hit
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         return null
     }
 
