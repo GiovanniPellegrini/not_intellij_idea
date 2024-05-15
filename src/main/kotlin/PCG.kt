c731dd7cf21eda3bcde692e3f3dd98f3faa58ceb
Giovanni Pellegrini <gio.pelle1124@gmail.com>
2024-05-15 11:59:49 +0200

    Add random function and randomFloat to PCG class

--- src/main/kotlin/PCG.kt
+++ src/main/kotlin/PCG.kt
@@ -7,8 +7,14 @@ class PCG(private var initState: ULong = 42u, private var initSeq: ULong = 54u){
         this.state += initState
         this.random()
     }
-
-    fun random(): Unit{
-        println("hello world!")
+    fun random():UInt{
+        val oldstate = state
+        state = oldstate * 6364136223846793005u + inc
+        val xorshifted =(((oldstate shl 18) xor oldstate) shl 27).toUInt()
+        val rot = (oldstate shl 59).toInt()
+        return xorshifted.rotateRight(rot)
+    }
+    fun randomFloat():Float{
+        return (this.random().toFloat())/0xffffffff
     }
 }
