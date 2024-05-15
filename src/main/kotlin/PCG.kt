af816086545a237caa897d6a5d5538b28f2ddd17
Giovanni Pellegrini <gio.pelle1124@gmail.com>
2024-05-15 12:16:47 +0200

    Fix random PCG function and add PCGTests

--- src/main/kotlin/PCG.kt
+++ src/main/kotlin/PCG.kt
@@ -10,8 +10,8 @@ class PCG(private var initState: ULong = 42u, private var initSeq: ULong = 54u){
     fun random():UInt{
         val oldstate = state
         state = oldstate * 6364136223846793005u + inc
-        val xorshifted =(((oldstate shl 18) xor oldstate) shl 27).toUInt()
-        val rot = (oldstate shl 59).toInt()
+        val xorshifted =(((oldstate shr 18) xor oldstate) shr 27).toUInt()
+        val rot = (oldstate shr 59).toInt()
         return xorshifted.rotateRight(rot)
     }
     fun randomFloat():Float{
--- src/test/kotlin/PCGTest.kt
+++ src/test/kotlin/PCGTest.kt
@@ -8,4 +8,24 @@ class PCGTest{
         println(pcg.state)
         println(pcg.inc)
     }
+
+    @Test
+    fun testPCG() {
+        val pcg = PCG()
+
+        assert(pcg.state == 1753877967969059832u)
+        assert(pcg.inc == 109.toULong())
+
+        val expectedValues = listOf<UInt>(
+            2707161783u,
+            2068313097u,
+            3122475824u,
+            2211639955u,
+            3215226955u,
+            3421331566u
+        )
+        for(expected in expectedValues) {
+           assert(expected==pcg.random())
+        }
+    }
 }
