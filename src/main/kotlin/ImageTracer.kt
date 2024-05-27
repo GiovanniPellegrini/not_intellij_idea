f7769b6e7d090168f30887c1750fb05a01b1105a
Giovanni Pellegrini <gio.pelle1124@gmail.com>
2024-05-27 10:10:19 +0200

    Add Antialiasing function, missing test

--- src/main/kotlin/ImageTracer.kt
+++ src/main/kotlin/ImageTracer.kt
@@ -2,7 +2,7 @@
  * Image tracer class. This class connects Camera objects with HdrImage objects
  */
 
-class ImageTracer(private var image: HdrImage, private var camera: Camera) {
+class ImageTracer(private val image: HdrImage, private val camera: Camera, private val pcg: PCG=PCG(), val raysForSide:Int=0) {
 
     /**
      * returns a Ray for a given position in the image
@@ -25,5 +25,29 @@ class ImageTracer(private var image: HdrImage, private var camera: Camera) {
             }
         }
     }
+
+    /**
+     *fire rays using Stratified Sampling. raysforSide indicates how many rays are fired for each side of the pixel
+     * In total will be fired raysforSideXraysforSide rays for each pixel
+     */
+    fun fireAllRays(func: (Ray) -> Color,pcg:PCG=PCG(),raysForSide: Int){
+        for(row in 0 until image.height) {
+            for(col in 0 until image.width) {
+                if(raysForSide==1) throw Error("number of ray must be greater than 0")
+
+                var cum=Color()
+                for(i in 0 until raysForSide){
+                    for(j in 0 until raysForSide){
+                        val uPixel= (i+pcg.randomFloat())/raysForSide
+                        val vPixel= (j+pcg.randomFloat())/raysForSide
+                        val ray=fireRay(col, row, uPixel, vPixel)
+                        cum+=func(ray)
+                    }
+                }
+                image.setPixel(col, row, cum*(1f/(raysForSide*raysForSide)))
+            }
+        }
+    }
+
 }
 
