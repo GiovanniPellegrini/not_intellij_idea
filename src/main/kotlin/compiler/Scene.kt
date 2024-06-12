c586bede878262df1d4a98d3a4f0b2d06f5d9be4
Giovanni Pellegrini <gio.pelle1124@gmail.com>
2024-06-12 12:21:40 +0200

    Add parseColor,parseVector,ParsePigment

--- src/main/kotlin/compiler/Scene.kt
+++ src/main/kotlin/compiler/Scene.kt
@@ -1,7 +1,20 @@
 package compiler
+
+import BRDF
 import Material
 import World
 import Camera
+import CheckeredPigment
+import Vector
+import Color
+import ImagePigment
+import Pigment
+import UniformPigment
+import readPfmImage
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.InputStream
+
 
 // to be developed
 class Scene(
@@ -10,13 +23,13 @@ class Scene(
     val camera: Camera? = null,
     val floatVariables: MutableMap<String, Float>,
     val overriddenVariables: MutableSet<String>
-){
+) {
     /**
      * Reads a token and checks if it is a symbol
      */
-    fun expectSymbol(inputFile: InStream, symbol: String){
+    fun expectSymbol(inputFile: InStream, symbol: String) {
         val token = inputFile.readToken()
-        if(token !is SymbolToken){
+        if (token !is SymbolToken) {
             throw GrammarError(token.location, "Expected symbol $symbol, but got $token")
         }
     }
@@ -25,12 +38,12 @@ class Scene(
      * Reads a token and checks if it is a keyword,
      * @return the keyword that was read as a KeyWordEnum
      */
-    fun expectKeywords(inputFile: InStream, keywords: List<KeyWordEnum>): KeyWordEnum{
+    fun expectKeyWords(inputFile: InStream, keywords: List<KeyWordEnum>): KeyWordEnum {
         val token = inputFile.readToken()
-        if(token !is KeyWordToken){
+        if (token !is KeyWordToken) {
             throw GrammarError(token.location, "Expected a keyword, but got $token")
         }
-        if(token.keywordEnum !in keywords){
+        if (token.keywordEnum !in keywords) {
             throw GrammarError(token.location, "Expected one of $keywords, but got ${token.keywordEnum}")
         }
         return token.keywordEnum
@@ -42,11 +55,11 @@ class Scene(
      */
     fun expectNumber(inputFile: InStream): Float {
         val token = inputFile.readToken()
-        if(token is LiteralNumberToken){
+        if (token is LiteralNumberToken) {
             return token.number
-        }else if(token is IdentifierToken){
+        } else if (token is IdentifierToken) {
             val variableName = token.identifier
-            if(variableName !in this.floatVariables){
+            if (variableName !in this.floatVariables) {
                 throw GrammarError(token.location, "Unknown variable $variableName")
             }
             return this.floatVariables[variableName]!!
@@ -54,13 +67,14 @@ class Scene(
         throw GrammarError(token.location, "Expected a number, but got $token")
     }
 
+
     /**
      * Reads a token and checks if it is a literal string
      * @return the string that was read
      */
     fun expectString(inputFile: InStream): String {
         val token = inputFile.readToken()
-        if(token is LiteralStringToken){
+        if (token is LiteralStringToken) {
             return token.string
         }
         throw GrammarError(token.location, "Expected a string, but got $token")
@@ -72,11 +86,73 @@ class Scene(
      */
     fun expectIdentifier(inputFile: InStream): String {
         val token = inputFile.readToken()
-        if(token is IdentifierToken){
+        if (token is IdentifierToken) {
             return token.identifier
         }
         throw GrammarError(token.location, "Expected an identifier, but got $token")
     }
 
+    fun parseVector(inStream: InStream): Vector {
+        expectSymbol(inStream, "<")
+        val x = expectNumber(inStream)
+        expectSymbol(inStream, ",")
+        val y = expectNumber(inStream)
+        expectSymbol(inStream, ",")
+        val z = expectNumber(inStream)
+        expectSymbol(inStream, ">")
 
+        return Vector(x, y, z)
+    }
+
+    fun parseColor(inStream: InStream): Color {
+        expectSymbol(inStream, "<")
+        val r = expectNumber(inStream)
+        expectSymbol(inStream, ",")
+        val g = expectNumber(inStream)
+        expectSymbol(inStream, ",")
+        val b = expectNumber(inStream)
+        expectSymbol(inStream, ">")
+
+        return Color(r, g, b)
+    }
+
+    fun parsePigment(inStream: InStream, scene: Scene): Pigment {
+        val keyWord =
+            expectKeyWords(inStream, listOf(KeyWordEnum.UNIFORM, KeyWordEnum.CHECKERED, KeyWordEnum.IMAGE))
+        expectSymbol(inStream, "(")
+
+        val result: Pigment = when (keyWord) {
+            KeyWordEnum.UNIFORM -> {
+                val color = parseColor(inStream)
+                UniformPigment(color)
+            }
+
+            KeyWordEnum.CHECKERED -> {
+                val color1 = parseColor(inStream)
+                expectSymbol(inStream, ",")
+                val color2 = parseColor(inStream)
+                expectSymbol(inStream, ",")
+                val numberOfSteps = expectNumber(inStream)
+                CheckeredPigment(color1, color2, numberOfSteps.toInt())
+            }
+
+            KeyWordEnum.IMAGE -> {
+                val string = expectString(inStream)
+                val stream = FileInputStream(string)
+                val image = readPfmImage(stream)
+                ImagePigment(image)
+            }
+
+            else -> throw Error("no clear definition of Pigment")
+        }
+
+        expectSymbol(inStream, ")")
+        return result
+    }
+
+    fun parseBrdf(inStream: InStream):BRDF{
+        TODO()
+    }
 }
+
+
