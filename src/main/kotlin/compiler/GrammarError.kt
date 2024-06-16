90cadd9cdf836cf77b088816359cff96e7e15be7
LorenzoEsposito <lorenzoesposito80@gmail.com>
2024-06-16 17:20:46 +0200

    added red underlining in Grammar error

--- src/main/kotlin/compiler/GrammarError.kt
+++ src/main/kotlin/compiler/GrammarError.kt
@@ -5,7 +5,7 @@ class GrammarError(val location: SourceLocation, message: String) : RuntimeExcep
     override fun toString(): String {
         val fileLines = File(location.fileName).readLines()
         val errorLine = fileLines[location.lineNumber - 1]
-        val underline = " ".repeat(location.columnNumber - 1) + "^"
+        val underline = " ".repeat(location.columnNumber - 1) + "\u001B[31m^^^\u001B[0m"
         return "\n$message \n$location \n\n$errorLine\n$underline"
     }
 }
