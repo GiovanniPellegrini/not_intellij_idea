d845c9cd0a0fb0c5457abe696f7db5db1b362577
LorenzoEsposito <lorenzoesposito80@gmail.com>
2024-06-17 16:33:03 +0200

    added further tests for sphere

--- src/test/kotlin/SphereTest.kt
+++ src/test/kotlin/SphereTest.kt
@@ -129,6 +129,34 @@ class SphereTest{
     }
 
     @Test
+    fun testUVCoordinates(){
+        val sphere=Sphere()
+        val ray1=Ray(Point(2f,0f,0f), Vector(-1f,0f,0f))
+        assertNotNull(sphere.rayIntersection(ray1))
+        assert(sphere.rayIntersection(ray1)!!.surfacePoint.isClose(Vec2d(0.0f,0.5f)))
+
+        val ray2=Ray(Point(0f,2f,0f), Vector(0f,-1f,0f))
+        assertNotNull(sphere.rayIntersection(ray2))
+        assert(sphere.rayIntersection(ray2)!!.surfacePoint.isClose(Vec2d(0.25f,0.5f)))
+
+        val ray3=Ray(Point(-2f,0f,0f), Vector(1f,0f,0f))
+        assertNotNull(sphere.rayIntersection(ray3))
+        assert(sphere.rayIntersection(ray3)!!.surfacePoint.isClose(Vec2d(0.5f,0.5f)))
+
+        val ray4=Ray(Point(0f,-2f,0f), Vector(0f,1f,0f))
+        assertNotNull(sphere.rayIntersection(ray4))
+        assert(sphere.rayIntersection(ray4)!!.surfacePoint.isClose(Vec2d(0.75f,0.5f)))
+
+        val ray5=Ray(Point(2f,0f,0.5f), Vector(-1f,0f,0f))
+        assertNotNull(sphere.rayIntersection(ray5))
+        assert(sphere.rayIntersection(ray5)!!.surfacePoint.isClose(Vec2d(0f,1/3f)))
+
+        val ray6=Ray(Point(2f,0f,-0.5f), Vector(-1f,0f,0f))
+        assertNotNull(sphere.rayIntersection(ray6))
+        assert(sphere.rayIntersection(ray6)!!.surfacePoint.isClose(Vec2d(0f,2/3f)))
+    }
+
+    @Test
     fun sphereListIntersection(){
         val sphere1=Sphere()
         val ray=Ray(Point(0f,0f,2f), Vector(0f,0f,-1f))
