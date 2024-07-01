dfe5d1a83471d282c4746250f56f120cffb2da11
LorenzoEsposito <lorenzoesposito80@gmail.com>
2024-07-01 12:01:24 +0200

    added gallery-images

--- animation.sh
+++ animation.sh
@@ -2,9 +2,9 @@ mkdir -p demo_frames
 
 for angle in $(seq 0 359); do
     angleNNN=$(printf "%03d" "$angle")
-    ./gradlew run --args="render -i demo.txt  -p demo_frames/img$angleNNN -a flatrender --declare-float angle=$angle"
+    ./gradlew run --args="render -i examples/demo/demo.txt  -p demo_frames/img$angleNNN -a flatrender --declare-float angle=$angle"
 done
 
-ffmpeg -r 25 -f image2 -s 720x720 -i demo_frames/img%03d.png \
+ffmpeg -r 25 -f image2 -s 480x480 -i demo_frames/img%03d.png \
     -vcodec libx264 -pix_fmt yuv420p \
     demo-animation.mp4
